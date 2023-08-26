8f97f176dfb4e912cae1f90836bf0722d0610c32
ciro <lanubedethom@gmail.com>
2023-08-26 11:57:28 -0500

    setting api route

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:3000/api/v1",
+    baseURL: 'https://restbook-8e57.onrender.com/api/v1',
+    //baseURL: "http://localhost:3000/api/v1",
 });
 
 export const loginRequest = async (user) => {
